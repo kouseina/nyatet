67df2e31607c00226669d90373845aca9494700f
kouseina <daffa@diyo.app>
2023-12-06 12:27:27 +0700

    feat(model): add note model

---
+++ lib/models/note.freezed.dart
@@ -0,0 +1,229 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'note.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+Note _$NoteFromJson(Map<String, dynamic> json) {
+  return _Note.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Note {
+  String? get title => throw _privateConstructorUsedError;
+  String? get desc => throw _privateConstructorUsedError;
+  @JsonKey(name: "create_at")
+  String? get createAt => throw _privateConstructorUsedError;
+  @JsonKey(name: "update_at")
+  String? get updateAt => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $NoteCopyWith<Note> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $NoteCopyWith<$Res> {
+  factory $NoteCopyWith(Note value, $Res Function(Note) then) =
+      _$NoteCopyWithImpl<$Res, Note>;
+  @useResult
+  $Res call(
+      {String? title,
+      String? desc,
+      @JsonKey(name: "create_at") String? createAt,
+      @JsonKey(name: "update_at") String? updateAt});
+}
+
+/// @nodoc
+class _$NoteCopyWithImpl<$Res, $Val extends Note>
+    implements $NoteCopyWith<$Res> {
+  _$NoteCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? title = freezed,
+    Object? desc = freezed,
+    Object? createAt = freezed,
+    Object? updateAt = freezed,
+  }) {
+    return _then(_value.copyWith(
+      title: freezed == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String?,
+      desc: freezed == desc
+          ? _value.desc
+          : desc // ignore: cast_nullable_to_non_nullable
+              as String?,
+      createAt: freezed == createAt
+          ? _value.createAt
+          : createAt // ignore: cast_nullable_to_non_nullable
+              as String?,
+      updateAt: freezed == updateAt
+          ? _value.updateAt
+          : updateAt // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$NoteImplCopyWith<$Res> implements $NoteCopyWith<$Res> {
+  factory _$$NoteImplCopyWith(
+          _$NoteImpl value, $Res Function(_$NoteImpl) then) =
+      __$$NoteImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String? title,
+      String? desc,
+      @JsonKey(name: "create_at") String? createAt,
+      @JsonKey(name: "update_at") String? updateAt});
+}
+
+/// @nodoc
+class __$$NoteImplCopyWithImpl<$Res>
+    extends _$NoteCopyWithImpl<$Res, _$NoteImpl>
+    implements _$$NoteImplCopyWith<$Res> {
+  __$$NoteImplCopyWithImpl(_$NoteImpl _value, $Res Function(_$NoteImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? title = freezed,
+    Object? desc = freezed,
+    Object? createAt = freezed,
+    Object? updateAt = freezed,
+  }) {
+    return _then(_$NoteImpl(
+      title: freezed == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String?,
+      desc: freezed == desc
+          ? _value.desc
+          : desc // ignore: cast_nullable_to_non_nullable
+              as String?,
+      createAt: freezed == createAt
+          ? _value.createAt
+          : createAt // ignore: cast_nullable_to_non_nullable
+              as String?,
+      updateAt: freezed == updateAt
+          ? _value.updateAt
+          : updateAt // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$NoteImpl with DiagnosticableTreeMixin implements _Note {
+  const _$NoteImpl(
+      {this.title,
+      this.desc,
+      @JsonKey(name: "create_at") this.createAt,
+      @JsonKey(name: "update_at") this.updateAt});
+
+  factory _$NoteImpl.fromJson(Map<String, dynamic> json) =>
+      _$$NoteImplFromJson(json);
+
+  @override
+  final String? title;
+  @override
+  final String? desc;
+  @override
+  @JsonKey(name: "create_at")
+  final String? createAt;
+  @override
+  @JsonKey(name: "update_at")
+  final String? updateAt;
+
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
+    return 'Note(title: $title, desc: $desc, createAt: $createAt, updateAt: $updateAt)';
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(DiagnosticsProperty('type', 'Note'))
+      ..add(DiagnosticsProperty('title', title))
+      ..add(DiagnosticsProperty('desc', desc))
+      ..add(DiagnosticsProperty('createAt', createAt))
+      ..add(DiagnosticsProperty('updateAt', updateAt));
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$NoteImpl &&
+            (identical(other.title, title) || other.title == title) &&
+            (identical(other.desc, desc) || other.desc == desc) &&
+            (identical(other.createAt, createAt) ||
+                other.createAt == createAt) &&
+            (identical(other.updateAt, updateAt) ||
+                other.updateAt == updateAt));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(runtimeType, title, desc, createAt, updateAt);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$NoteImplCopyWith<_$NoteImpl> get copyWith =>
+      __$$NoteImplCopyWithImpl<_$NoteImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$NoteImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _Note implements Note {
+  const factory _Note(
+      {final String? title,
+      final String? desc,
+      @JsonKey(name: "create_at") final String? createAt,
+      @JsonKey(name: "update_at") final String? updateAt}) = _$NoteImpl;
+
+  factory _Note.fromJson(Map<String, dynamic> json) = _$NoteImpl.fromJson;
+
+  @override
+  String? get title;
+  @override
+  String? get desc;
+  @override
+  @JsonKey(name: "create_at")
+  String? get createAt;
+  @override
+  @JsonKey(name: "update_at")
+  String? get updateAt;
+  @override
+  @JsonKey(ignore: true)
+  _$$NoteImplCopyWith<_$NoteImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
---
+++ lib/models/note.g.dart
@@ -0,0 +1,22 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'note.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$NoteImpl _$$NoteImplFromJson(Map<String, dynamic> json) => _$NoteImpl(
+      title: json['title'] as String?,
+      desc: json['desc'] as String?,
+      createAt: json['create_at'] as String?,
+      updateAt: json['update_at'] as String?,
+    );
+
+Map<String, dynamic> _$$NoteImplToJson(_$NoteImpl instance) =>
+    <String, dynamic>{
+      'title': instance.title,
+      'desc': instance.desc,
+      'create_at': instance.createAt,
+      'update_at': instance.updateAt,
+    };
